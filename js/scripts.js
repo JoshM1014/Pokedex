d17c708222f8a0c221be2934b6ceaa2504202116
JoshM1014 <79695995+JoshM1014@users.noreply.github.com>
2021-03-12 00:11:58 -0600

    Add pokemonList variable with pokemon items
    Added a pokemonList variable, filled out with 3 types of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 /* console.log() & document.write() */
+let pokemonList = [
+    {name: 'bulbasaur', height: '0.7m', type: 'grass'},
+    {name: 'charmander', height: '0.6m', type: 'fire'},
+    {name: 'squirtle', height: '0.5m', type: 'water'}
+];
