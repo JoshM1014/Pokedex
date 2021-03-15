717197992825c72db70c2abbf498d734ec24106d
JoshM1014 <79695995+JoshM1014@users.noreply.github.com>
2021-03-14 22:33:35 -0500

    1.3 add pokemonList loop display
    Created a loop to display each object on pokemonList, along with extra comment for large pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,14 @@ let pokemonList = [
     {name: 'charmander', height: '0.6', type: 'fire'},
     {name: 'squirtle', height: '0.5', type: 'water'}
 ];
+    //printArrayDetails funcion declaration
+function printArrayDetails(){
+    // loop to display pokemonList */
+    for (let i=0; i < pokemonList.length; i++){
+        if(pokemonList[i].height <= 0.6)
+    document.write(pokemonList[i].name + " \(height: " + pokemonList[i].height + "m\)\, " + "<br />")
+        else{
+        document.write(pokemonList[i].name + " \(height: " + pokemonList[i].height + "m\)\ " + "-Wow, that's BIG," + "<br />")
+        }
     }
 }
