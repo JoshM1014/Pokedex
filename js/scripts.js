5d634abdb50a2122e58b51d3939b0619b95c34dc
JoshM1014 <79695995+JoshM1014@users.noreply.github.com>
2021-03-14 22:31:03 -0500

    add comment for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 /* console.log() & document.write() */
+// pokemon object 
 let pokemonList = [
     {name: 'bulbasaur', height: '0.7', type: 'grass'},
     {name: 'charmander', height: '0.6', type: 'fire'},
