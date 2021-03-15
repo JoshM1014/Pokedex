0fa02bab3d51eef49fd974c1692aa0eab2481b69
JoshM1014 <79695995+JoshM1014@users.noreply.github.com>
2021-03-14 22:31:56 -0500

    clear script.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,5 @@ let pokemonList = [
     {name: 'charmander', height: '0.6', type: 'fire'},
     {name: 'squirtle', height: '0.5', type: 'water'}
 ];
-/* loop to display pokemonList */
-for (let i=0; i < pokemonList.length; i++){
-    if(pokemonList[i].height <= 0.6)
-document.write(pokemonList[i].name + " \(height: " + pokemonList[i].height + "m\)\, " + "<br />")
-    else{
-    document.write(pokemonList[i].name + " \(height: " + pokemonList[i].height + "m\)\ " + "-Wow, that's BIG," + "<br />")
     }
 }
