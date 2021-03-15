976874b7c29ed2c337e5580bd606a9890ae52638
JoshM1014 <79695995+JoshM1014@users.noreply.github.com>
2021-03-14 19:07:50 -0500

    fix array numbers to be integers
    Changed height numbers to integers without measurement

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 /* console.log() & document.write() */
 let pokemonList = [
+    {name: 'bulbasaur', height: '0.7', type: 'grass'},
+    {name: 'charmander', height: '0.6', type: 'fire'},
+    {name: 'squirtle', height: '0.5', type: 'water'}
 ];
