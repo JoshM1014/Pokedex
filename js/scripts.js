874bac3b18e9ce49953096e506a437232e69cd8c
JoshM1014 <79695995+JoshM1014@users.noreply.github.com>
2021-03-14 19:07:15 -0500

    fix array numbers to be integers

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
 /* console.log() & document.write() */
 let pokemonList = [
-    {name: 'bulbasaur', height: '0.7m', type: 'grass'},
-    {name: 'charmander', height: '0.6m', type: 'fire'},
-    {name: 'squirtle', height: '0.5m', type: 'water'}
 ];
