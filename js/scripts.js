c9dd435619bef738d5977c6d18bd267b15dbc75e
JoshM1014 <79695995+JoshM1014@users.noreply.github.com>
2021-03-17 15:36:30 -0500

    1.5 task help

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,44 @@
-/* console.log() & document.write() */
-// pokemon object 
-let pokemonList = [
-    {name: 'bulbasaur', height: '0.7', type: 'grass'},
-    {name: 'charmander', height: '0.6', type: 'fire'},
-    {name: 'squirtle', height: '0.5', type: 'water'}
-];
-    //printArrayDetails funcion declaration
+// pokemon IIFE repository list
+let pokemonRepository = (function (){
+   let pokemonList = [
+        {name: 'bulbasaur', height: '0.7', type: 'grass'},
+        {name: 'charmander', height: '0.6', type: 'fire'},
+        {name: 'squirtle', height: '0.5', type: 'water'}
+    ];
+
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
+
+        function getAll() {
+            return pokemonList;
+        }
+        return {
+            add: add,
+            getAll: getAll
+        };
+})();
+
+console.log(pokemonRepository.getAll());
+
+// forEach() Loop with the declaration moved 
+ function showPokemon(pokemon) {
+    document.write(pokemon.name + ', is ' + pokemon.height + 'm, ' + pokemon.type + '.' + '<br />');
+} 
+pokemonList.forEach(showPokemon);
+
+
+
+
+
+
+
+
+
+/*
+//printArrayDetails funcion declaration
 function printArrayDetails(){
-    // loop to display pokemonList */
+    // loop to display pokemonList 
     for (let i=0; i < pokemonList.length; i++){
         if(pokemonList[i].height <= 0.6)
     document.write(pokemonList[i].name + " \(height: " + pokemonList[i].height + "m\)\, " + "<br />")
@@ -15,4 +46,5 @@ function printArrayDetails(){
         document.write(pokemonList[i].name + " \(height: " + pokemonList[i].height + "m\)\ " + "-Wow, that's BIG," + "<br />")
         }
     }
-}
+} 
+*/
